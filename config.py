3d9d423af5a0a137b2fa594c83c6bb2a2b1a97f7
Platon <platonovza@ya.ru>
2017-11-20 17:20:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # TODO: переделать на парсинг текстового файла из ~/.config/
-LOGIN = ""
-PASS = ""
-URL = ""
-TIMEOUT = 10
+LOGIN = "" # Пользователь BILLmanager
+PASS = ""  # Пароль
+URL = ""   # Адрес BILLmanager (e.g. https://10.0.0.2:1500/billmgr)
+TIMEOUT = 10 # Время между запросами к BILLmanager
